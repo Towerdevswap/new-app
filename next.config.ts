791a774c5c7f15bee06119aa7171f95ede623831
Towerswap Finance <106070857+Towerdevswap@users.noreply.github.com>
2025-04-27 15:15:46 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
-import withPWA from 'next-pwa';
-
-export default withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  },
+const nextConfig = {
   reactStrictMode: true,
-});
+  webpack(config, { isServer }) {
+    // Mengatasi import yang menggunakan window di server
+    if (isServer) {
+      config.externals.push('window');
+    }
+
+    // Only apply PWA-specific settings during client-side builds
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
